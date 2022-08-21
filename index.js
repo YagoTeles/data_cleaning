ba80d3ed9e37f8e1eab1db919e85393755b3c0a7
Yago Teles <yago.teles@hotmail.com>
2022-08-21 00:42:23 -0300

    Feature in Date

--- index.js
+++ index.js
@@ -19,6 +19,32 @@ fs.createReadStream("./Base_de_Alunos5.csv")
 
 const handledata = (row) => {
   if(row === null || undefined) return ''
-  const obj = [{id: row[0],nome:row[1],nome_da_mae:row[2],nome_do_pai:row[3],sexo:row[4],data_de_nascimento:row[5]}]
+  const obj = {
+                id: row[0],
+                nome:row[1],
+                nome_da_mae:row[2],
+                nome_do_pai:row[3],
+                sexo:row[4].toUpperCase(),
+                data_de_nascimento: newDate(row[5]),
+              }
   return obj
+}
+
+const newDate = (date) => {
+  if(date === null || undefined) return ''
+
+  const currentDate = new Date();
+  const nDate = date.replace(/-/g, "/").split(" ")[0].split("/");
+  let day = nDate[0];
+  let month = nDate[1];
+  let year = nDate[2];
+  if (parseInt(day) > 31 || parseInt(month) > 12 || parseInt(year) > currentDate.getFullYear()) return ''
+
+  else{
+    if(day.length === 1) day = '0' + day
+    if(month.length === 1) month = '0' + month
+    return [day,month,year].join("/");
+  }
+   
+  
 }
